f98666332d56793ab76ffca17077c85dad04d832
opoyc <opoyserc@gmail.com>
2019-04-03 18:32:20 +0200

    commit

--- test.R
+++ test.R
@@ -1,1 +1,5 @@
-wdi
+grid.arrange(x$k_graph[[1]], x$k_graph[[2]]
+             , x$k_graph[[3]], x$k_graph[[4]]
+             , x$k_graph[[5]], x$k_graph[[6]] 
+             , x$k_graph[[7]], x$k_graph[[8]]
+             , x$k_graph[[9]])
