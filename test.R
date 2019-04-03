6936613eb73a2e8dd794c897ed620379c6269603
opoyc <opoyserc@gmail.com>
2019-04-04 01:57:55 +0200

    commit

--- test.R
+++ test.R
@@ -1,5 +1,11 @@
-grid.arrange(x$k_graph[[1]], x$k_graph[[2]]
-             , x$k_graph[[3]], x$k_graph[[4]]
-             , x$k_graph[[5]], x$k_graph[[6]] 
-             , x$k_graph[[7]], x$k_graph[[8]]
-             , x$k_graph[[9]])
+pca_var$contrib %>% 
+    as.data.frame() %>% 
+    tibble::rownames_to_column("obs") %>% 
+    janitor::clean_names() %>% 
+    pivot_longer(cols = dim_1:dim_6, names_to = "comp") %>% 
+    group_by(comp) %>% 
+    top_n(n = 3, wt = value) %>% 
+    
+    ggplot(aes(reorder(obs, value), value, fill=comp))+
+    geom_col(position = "dodge")+
+    coord_flip()
